4baed6c3f8809d4d2a4c632aacb99eed3bb31e85
Andres Salamanca <andrealebarbaritos@gmail.com>
2024-11-24 17:42:18 -0500

    otros cambios

--- setup.bat
+++ setup.bat
@@ -7,36 +7,10 @@ if %errorlevel% equ 0 (
     echo Conda ya esta instalado.
     goto activate_env
 ) else (
-    echo Conda no esta instalado. Procediendo con la instalacion...
+    echo Conda no esta instalado. Por favor instalar https://docs.anaconda.com/miniconda/install/
+exit /b 1
 )
 
-REM Descargar e instalar Miniconda
-curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe -o miniconda.exe
-if %errorlevel% neq 0 (
-    echo Error al descargar Miniconda. Verifica tu conexión a internet.
-    exit /b 1
-)
-
-start /wait "" .\miniconda.exe /S /D=%USERPROFILE%\Miniconda3
-if %errorlevel% neq 0 (
-    echo Error al instalar Miniconda.
-    del miniconda.exe
-    exit /b 1
-)
-
-
-echo Miniconda instalado exitosamente.
-REM Agregar Miniconda al PATH temporalmente para esta sesión
-set "PATH=%USERPROFILE%\Miniconda3;%USERPROFILE%\Miniconda3\Scripts;%USERPROFILE%\Miniconda3\Library\bin;%PATH%"
-
-
-
-REM Verificar la instalación de Conda
-where conda >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Error: No se pudo encontrar Conda después de la instalacion.
-    exit /b 1
-)
 
 :activate_env
 echo Creando el entorno Conda...
@@ -62,10 +36,10 @@ python manage.py createsuperuser
 python install.py
 cd ..\java\UranusCB-V2.0\
 mvn clean package
-chmod +x launch.sh
+chmod +x launch.cmd
 cd ..\uranus\
 python manage.py runserver
 
-echo Configuración completa. Servidor iniciado exitosamente.
+echo Configuracion completa. Servidor iniciado exitosamente.
 exit /b 0
 
